b12f96085e4f58fdb9da4d490d7eac035495aa2f
Arnold Vink <info@arnoldvink.com>
2025-05-02 10:36:28 +0200

    - Added UIAutomation test code.

--- KeyboardOpener/DetectKeyboard.cs
+++ KeyboardOpener/DetectKeyboard.cs
@@ -1,14 +1,17 @@
 ﻿using System;
-using System.Configuration;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
+using System.Windows.Automation;
+using static Win8_KeyboardOpener.ManageKeyboard;
 
 namespace Win8_KeyboardOpener
 {
     class DetectKeyboard
     {
+        //Dll Imports
         [DllImport("kernel32.dll")]
         static extern IntPtr GetCurrentThreadId();
 
@@ -27,18 +30,9 @@ namespace Win8_KeyboardOpener
         [DllImport("user32.dll")]
         static extern IntPtr AttachThreadInput(IntPtr AttachId, IntPtr AttachToId, bool AttachStatus);
 
-        [DllImport("user32.dll")]
-        static extern IntPtr FindWindow(string ClassName, string WindowName);
-
-        [DllImport("user32.dll")]
-        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
-
         //Application Variables
-        Process Process = new Process();
-        StringBuilder StringBuilder = new StringBuilder(50);
-        static public int ActiveProcessFocusId = 0;
-        static public string ActiveProcessFocus = "";
-        static public string PreviousProcessFocus = "";
+        static public string vActiveProcessFocus = "";
+        static public string vPreviousProcessFocus = "";
 
         //Detect Keyboard Requests
         public void DetectKeyBoard()
@@ -47,40 +41,126 @@ namespace Win8_KeyboardOpener
             {
                 try
                 {
-                    AttachThreadInput(GetWindowThreadProcessId(GetForegroundWindow(), IntPtr.Zero), GetCurrentThreadId(), true);
-                    IntPtr GetFocusId = GetFocus();
-                    AttachThreadInput(GetWindowThreadProcessId(GetForegroundWindow(), IntPtr.Zero), GetCurrentThreadId(), false);
-                    if (GetClassName(GetFocusId, StringBuilder, StringBuilder.Capacity))
+                    //bool openKeyboard = DetectTextbox_ClassName();
+                    bool openKeyboard = DetectTextbox_UIAutomation();
+
+                    //Fix manually opened keyboard closing
+                    if (vPreviousProcessFocus != vActiveProcessFocus)
                     {
-                        ActiveProcessFocus = StringBuilder.ToString();
-                        if (PreviousProcessFocus != ActiveProcessFocus && !ActiveProcessFocus.Contains("KeyboardOpener"))
+                        continue;
+                    }
+
+                    if (openKeyboard)
+                    {
+                        OpenKeyboard();
+                    }
+                    else
+                    {
+                        CloseKeyboard();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Failed detecting keyboard request: " + ex.Message);
+                }
+                finally
+                {
+                    Thread.Sleep(500);
+                }
+            }
+        }
+
+        private static bool DetectTextbox_ClassName()
+        {
+            try
+            {
+                bool openKeyboard = false;
+                StringBuilder stringBuilder = new StringBuilder(50);
+
+                AttachThreadInput(GetWindowThreadProcessId(GetForegroundWindow(), IntPtr.Zero), GetCurrentThreadId(), true);
+                IntPtr focusedHandle = GetFocus();
+                AttachThreadInput(GetWindowThreadProcessId(GetForegroundWindow(), IntPtr.Zero), GetCurrentThreadId(), false);
+
+                if (GetClassName(focusedHandle, stringBuilder, stringBuilder.Capacity))
+                {
+                    vActiveProcessFocus = stringBuilder.ToString();
+                    if (vPreviousProcessFocus != vActiveProcessFocus && !vActiveProcessFocus.Contains("KeyboardOpener"))
+                    {
+                        if (vActiveProcessFocus == "Edit" || vActiveProcessFocus == "SearchPane" || vActiveProcessFocus.Contains("RichEdit") || vActiveProcessFocus.Contains("SearchEdit") || vActiveProcessFocus.Contains("TextfieldEdit") || vActiveProcessFocus.Contains("Afx:00400000") || vActiveProcessFocus == "_WwG" || vActiveProcessFocus == "Scintilla" || vActiveProcessFocus == "SPEAD0C4")
                         {
-                            if (ActiveProcessFocus == "Edit" || ActiveProcessFocus == "SearchPane" || ActiveProcessFocus.Contains("RichEdit") || ActiveProcessFocus.Contains("SearchEdit") || ActiveProcessFocus.Contains("TextfieldEdit") || ActiveProcessFocus.Contains("Afx:00400000") || ActiveProcessFocus == "_WwG" || ActiveProcessFocus == "Scintilla" || ActiveProcessFocus == "SPEAD0C4")
-                            {
-                                try
-                                {
-                                    //Open TabTip / On Screen Keyboard
-                                    if (ConfigurationManager.AppSettings["KeyboardType"] == "0") { Process.Start("C:\\Program Files\\Common Files\\Microsoft Shared\\ink\\TabTip.exe"); }
-                                    else { Process.Start("C:\\Windows\\System32\\osk.exe"); }
-                                }
-                                catch { }
-                            }
-                            else
-                            {
-                                try
-                                {
-                                    //Close TabTip / On Screen Keyboard
-                                    SendMessage(FindWindow("IPTip_Main_Window", null), 0x0112, (IntPtr)0xF060, IntPtr.Zero);
-                                    foreach (Process KillProc in Process.GetProcessesByName("osk")) { KillProc.Kill(); }
-                                }
-                                catch { }
-                            }
-                            PreviousProcessFocus = ActiveProcessFocus;
+                            openKeyboard = true;
                         }
                     }
-                    Thread.Sleep(250);
                 }
-                catch { }
+
+                return openKeyboard;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed to detect textbox: " + ex.Message);
+                return false;
+            }
+        }
+
+        private static bool DetectTextbox_UIAutomation()
+        {
+            try
+            {
+                bool openKeyboard = false;
+
+                IntPtr focusedHandle = GetFocus();
+                if (focusedHandle == IntPtr.Zero)
+                {
+                    focusedHandle = GetForegroundWindow();
+                }
+
+                AutomationElement autoElement = AutomationElement.FromHandle(focusedHandle);
+                //Fix FindAll seems to freeze for couple seconds on certain processes
+                AutomationElementCollection controlEdit = autoElement.FindAll(TreeScope.Subtree, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit));
+                AutomationElementCollection controlPane = autoElement.FindAll(TreeScope.Subtree, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Pane));
+                AutomationElementCollection controlDocument = autoElement.FindAll(TreeScope.Subtree, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Document));
+                AutomationElementCollection controlComboBox = autoElement.FindAll(TreeScope.Subtree, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.ComboBox));
+
+                bool focusedEdit = controlEdit.Cast<AutomationElement>().ToList().Any(x => x.Current.HasKeyboardFocus);
+                if (focusedEdit) { openKeyboard = true; }
+
+                bool focusedPane = controlPane.Cast<AutomationElement>().ToList().Where(x => x.Current.ClassName.Contains("Scintilla") && x.Current.HasKeyboardFocus).Any();
+                if (focusedPane) { openKeyboard = true; }
+
+                bool focusedDocument = controlDocument.Cast<AutomationElement>().ToList().Where(x => x.Current.ClassName.Contains("Edit") && x.Current.HasKeyboardFocus).Any();
+                if (focusedDocument) { openKeyboard = true; }
+
+                bool focusedComboBox = controlComboBox.Cast<AutomationElement>().ToList().Any(x => x.Current.HasKeyboardFocus);
+                if (focusedComboBox) { openKeyboard = true; }
+
+                Debug.WriteLine("Edit controls: " + controlEdit.Count + " / " + focusedEdit);
+                Debug.WriteLine("Pane controls: " + controlPane.Count + " / " + focusedPane);
+                Debug.WriteLine("Document controls: " + controlDocument.Count + " / " + focusedDocument);
+                Debug.WriteLine("ComboBox controls: " + controlComboBox.Count + " / " + focusedComboBox);
+
+                ////Show debug information
+                //var combined = controlEdit.Cast<AutomationElement>().ToList().Concat(controlDocument.Cast<AutomationElement>().ToList()).Concat(controlComboBox.Cast<AutomationElement>().ToList());
+                //if (combined.Count() > 0)
+                //{
+                //    foreach (AutomationElement element in combined)
+                //    {
+                //        if (element != null && element.Current.HasKeyboardFocus)
+                //        {
+                //            Debug.WriteLine("Name: " + element.Current.Name);
+                //            Debug.WriteLine("ItemType: " + element.Current.ItemType);
+                //            Debug.WriteLine("ClassName: " + element.Current.ClassName);
+                //            Debug.WriteLine("HasKeyboardFocus: " + element.Current.HasKeyboardFocus);
+                //            Debug.WriteLine("ProgrammaticName: " + element.Current.ControlType.ProgrammaticName);
+                //        }
+                //    }
+                //}
+
+                return openKeyboard;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed to detect textbox: " + ex.Message);
+                return false;
             }
         }
     }
